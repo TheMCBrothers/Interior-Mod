494df7b0ff99e1ece2005cc7671d826384b4bd61
TheMCLoveMan <themcloveman@gmail.com>
2021-04-12 21:31:50 +0200

    Tooltip system

--- src/main/java/tk/themcbros/interiormod/init/InteriorItems.java
+++ src/main/java/tk/themcbros/interiormod/init/InteriorItems.java
@@ -11,8 +11,8 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import tk.themcbros.interiormod.InteriorMod;
 import tk.themcbros.interiormod.api.furniture.FurnitureType;
-import tk.themcbros.interiormod.items.DescBlockItem;
-import tk.themcbros.interiormod.items.DescTallBlockItem;
+import tk.themcbros.interiormod.items.TooltipBlockItem;
+import tk.themcbros.interiormod.items.TooltipTallBlockItem;
 import tk.themcbros.interiormod.items.FurnitureBlockItem;
 
 /**
@@ -24,12 +24,12 @@ public class InteriorItems {
 
     public static final BlockItem CHAIR = registerItem("chair", new FurnitureBlockItem(FurnitureType.CHAIR, new Item.Properties().group(InteriorItemGroup.INSTANCE)));
     public static final BlockItem TABLE = registerItem("table", new FurnitureBlockItem(FurnitureType.TABLE, new Item.Properties().group(InteriorItemGroup.INSTANCE)));
-    public static final BlockItem FRIDGE = registerItem("fridge", new DescTallBlockItem(InteriorBlocks.FRIDGE, new Item.Properties().group(InteriorItemGroup.INSTANCE)));
-    public static final BlockItem LAMP = registerItem("lamp", new DescBlockItem(InteriorBlocks.LAMP, new Item.Properties().group(InteriorItemGroup.INSTANCE)));
-    public static final BlockItem LAMP_ON_A_STICK = registerItem("lamp_on_a_stick", new DescTallBlockItem(InteriorBlocks.LAMP_ON_A_STICK, new Item.Properties().group(InteriorItemGroup.INSTANCE)));
-    public static final BlockItem TRASH_CAN = registerItem("trash_can", new DescTallBlockItem(InteriorBlocks.TRASH_CAN, new Item.Properties().group(InteriorItemGroup.INSTANCE)));
-    public static final BlockItem MODERN_DOOR = registerItem("modern_door", new DescTallBlockItem(InteriorBlocks.MODERN_DOOR, new Item.Properties().group(InteriorItemGroup.INSTANCE)));
-    public static final BlockItem FURNITURE_WORKBENCH = registerItem("furniture_workbench", new DescBlockItem(InteriorBlocks.FURNITURE_WORKBENCH, new Item.Properties().group(InteriorItemGroup.INSTANCE)));
+    public static final BlockItem FRIDGE = registerItem("fridge", new TooltipTallBlockItem(InteriorBlocks.FRIDGE, new Item.Properties().group(InteriorItemGroup.INSTANCE)));
+    public static final BlockItem LAMP = registerItem("lamp", new TooltipBlockItem(InteriorBlocks.LAMP, new Item.Properties().group(InteriorItemGroup.INSTANCE)));
+    public static final BlockItem LAMP_ON_A_STICK = registerItem("lamp_on_a_stick", new TooltipTallBlockItem(InteriorBlocks.LAMP_ON_A_STICK, new Item.Properties().group(InteriorItemGroup.INSTANCE)));
+    public static final BlockItem TRASH_CAN = registerItem("trash_can", new TooltipTallBlockItem(InteriorBlocks.TRASH_CAN, new Item.Properties().group(InteriorItemGroup.INSTANCE)));
+    public static final BlockItem MODERN_DOOR = registerItem("modern_door", new TooltipTallBlockItem(InteriorBlocks.MODERN_DOOR, new Item.Properties().group(InteriorItemGroup.INSTANCE)));
+    public static final BlockItem FURNITURE_WORKBENCH = registerItem("furniture_workbench", new TooltipBlockItem(InteriorBlocks.FURNITURE_WORKBENCH, new Item.Properties().group(InteriorItemGroup.INSTANCE)));
 
     private static <T extends Item> T registerItem(String registryName, T item) {
         item.setRegistryName(InteriorMod.getId(registryName));
