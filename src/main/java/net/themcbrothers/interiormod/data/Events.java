fe8dc2e5c9c3f0ee043939ac77608c59be515753
TheMCLoveMan <themcloveman@gmail.com>
2023-06-18 20:14:11 +0200

    generate pack.mcmeta

--- src/main/java/net/themcbrothers/interiormod/data/Events.java
+++ src/main/java/net/themcbrothers/interiormod/data/Events.java
@@ -1,8 +1,13 @@
 package net.themcbrothers.interiormod.data;
 
+import net.minecraft.DetectedVersion;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
+import net.minecraft.data.metadata.PackMetadataGenerator;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.packs.PackType;
+import net.minecraft.server.packs.metadata.pack.PackMetadataSection;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.common.data.ForgeAdvancementProvider;
 import net.minecraftforge.data.event.GatherDataEvent;
@@ -10,8 +15,11 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.themcbrothers.interiormod.api.InteriorAPI;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * @author TheMCBrothers
@@ -31,5 +39,12 @@ public class Events {
         generator.addProvider(event.includeServer(), new RecipeDataProvider(packOutput));
         generator.addProvider(event.includeServer(), new ForgeAdvancementProvider(packOutput, lookupProvider, existingFileHelper, List.of(new InteriorAdvancementGenerator())));
         generator.addProvider(event.includeServer(), new InteriorBlockTagsProvider(packOutput, lookupProvider, existingFileHelper));
+
+        // pack.mcmeta
+        generator.addProvider(true, new PackMetadataGenerator(packOutput))
+                .add(PackMetadataSection.TYPE,
+                        new PackMetadataSection(Component.literal("TheMCBrothers Interior Mod"),
+                                DetectedVersion.BUILT_IN.getPackVersion(PackType.CLIENT_RESOURCES),
+                                Arrays.stream(PackType.values()).collect(Collectors.toMap(Function.identity(), DetectedVersion.BUILT_IN::getPackVersion))));
     }
 }
