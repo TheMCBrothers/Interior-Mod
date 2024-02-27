de1704f8d34a64c6c4491fd068bb1130bca49fc9
TheMCLoveMan <themcloveman@gmail.com>
2024-02-27 01:41:38 +0100

    clean up usage of RecipeManager

--- src/main/java/net/themcbrothers/interiormod/compat/jei/InteriorRecipes.java
+++ src/main/java/net/themcbrothers/interiormod/compat/jei/InteriorRecipes.java
@@ -2,15 +2,11 @@ package net.themcbrothers.interiormod.compat.jei;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientLevel;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.Container;
 import net.minecraft.world.item.crafting.CraftingRecipe;
-import net.minecraft.world.item.crafting.Recipe;
 import net.minecraft.world.item.crafting.RecipeManager;
-import net.minecraft.world.item.crafting.RecipeType;
 import net.themcbrothers.interiormod.init.InteriorRecipeTypes;
 
-import java.util.*;
+import java.util.List;
 
 /**
  * @author TheMCBrothers
@@ -25,12 +21,6 @@ public class InteriorRecipes {
     }
 
     public List<CraftingRecipe> getFurnitureCraftingRecipes() {
-        return new ArrayList<>(getRecipes(recipeManager, InteriorRecipeTypes.FURNITURE_CRAFTING.get()));
-    }
-
-    @SuppressWarnings("unchecked")
-    private <C extends Container, T extends Recipe<C>> Collection<T> getRecipes(RecipeManager recipeManager, RecipeType<T> recipeType) {
-        Map<ResourceLocation, Recipe<?>> recipes = recipeManager.recipes.getOrDefault(recipeType, Collections.emptyMap());
-        return (Collection<T>) recipes.values();
+        return this.recipeManager.getAllRecipesFor(InteriorRecipeTypes.FURNITURE_CRAFTING.get());
     }
 }
